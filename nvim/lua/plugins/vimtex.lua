40e293a72a3023cfbd0ac5f0b735023643170ca5
ThatAmuzak <rishavbanerjee1710@gmail.com>
2026-02-09 16:31:04 -0800

    moved to Sioyek
    this approach is cursed, but apparently does the job

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -3,9 +3,17 @@ return {
 		"lervag/vimtex",
 		lazy = false,
 		init = function()
-			vim.g.vimtex_view_general_viewer = "SumatraPDF"
-			vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
-			vim.g.vimtex_syntax_enabled = 1
+			vim.cmd("syntax enable")
+			vim.cmd("let g:vimtex_view_general_viewer='sioyek'")
+			-- %1: file, %2: line number
+			local options = string.format(
+				'--reuse-window --inverse-search="nvr --servername %s +%%2 %%1" --forward-search-file @tex --forward-search-line @line @pdf',
+				vim.v.servername
+			)
+
+			local command = string.format("let g:vimtex_view_general_options='%s'", options)
+			vim.cmd(command)
+			vim.cmd("let g:vimtex_compiler_progname='nvr'")
 		end,
 	},
 
