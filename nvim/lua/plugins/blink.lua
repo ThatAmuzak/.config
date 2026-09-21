7ca15d5902b5324df3c0db0feda93f4d20145415
ThatAmuzak <rishavbanerjee1710@gmail.com>
2026-09-21 11:27:30 -0700

    adding the dependency for blink

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,14 +1,17 @@
 -- blink.cmp: buffer-words-only completion
 return {
-  {
-    "saghen/blink.cmp",
-    opts = function(_, opts)
-      opts.sources = opts.sources or {}
-      -- replace (not merge) LazyVim's default source list
-      opts.sources.default = { "buffer" }
-      -- disable cmdline completion (schema: top-level cmdline, sibling of sources)
-      opts.cmdline = vim.tbl_deep_extend("force", opts.cmdline or {}, { sources = {} })
-      return opts
-    end,
-  },
+    {
+        "saghen/blink.cmp",
+        dependencies = { "saghen/blink.lib" },
+        build = function() require('blink.cmp').build():pwait() end,
+        opts = function(_, opts)
+            opts.sources = opts.sources or {}
+            -- replace (not merge) LazyVim's default source list
+            opts.sources.default = { "buffer" }
+            opts.sources.per_filetype = {}
+            -- disable cmdline completion (schema: top-level cmdline, sibling of sources)
+            opts.cmdline = vim.tbl_deep_extend("force", opts.cmdline or {}, { sources = {} })
+            return opts
+        end,
+    },
 }
